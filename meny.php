50cd22ba4014cabeb15c0614b48928ceaa49e292
Sep <seb18061@skole.telemarkfylke.no>
2026-03-13 12:22:06 +0100

    Update navigation links and last modified timestamp in meny.php

--- meny.php
+++ meny.php
@@ -3,14 +3,16 @@
  * @Author: Sep-aa
  * @Date:   2026-03-03 10:19:35
  * @Last Modified by:   Sep-aa
- * @Last Modified time: 2026-03-03 10:30:52
+ * @Last Modified time: 2026-03-13 12:21:44
  */
 
     echo '
     <nav>
         <ul>
-            <li><a href="ny_ansatte.php">Ny</a></li>
-            <li><a href="les_ansatte.php">Les</a></li>
+            <li><a href="ny_ansatte.php">Ny ansatt</a></li>
+            <li><a href="les_ansatte.php">Les ansatte</a></li>
+            <li><a href="ny_kontaktperson.php">Ny kontaktperson</a></li>
+            <li><a href="les_kontaktperson.php">Les kontaktpersoner</a></li>
         </ul>
     </nav>';
 
